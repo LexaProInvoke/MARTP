3dec66d6d86becc627112298451947e85f6cb8c7
Kateryna <katuxashcherbakova@gmail.com>
2023-12-04 16:53:03 +0100

    task 3 begin

---
+++ Assets/Scripts/ZoneManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneManager : MonoBehaviour
+{
+    public int CollectibleCount = 0; // number of pickups for each zone
+    public GameObject Door; // obstacle to open after finishing the zone 
+
+    void Start()
+    {
+
+    }
+
+}
